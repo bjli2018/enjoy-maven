a5a72637060d65e3034ae414f21a391a934853de
dkq6686 <Bob.Li@varian.com>
2019-09-17 15:02:06 +0800

    new project

--- src/main/java/com/enjoy/controller/TestController.java
+++ src/main/java/com/enjoy/controller/TestController.java
@@ -3,6 +3,7 @@ package com.enjoy.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//
 @RestController
 @RequestMapping("/test")
 public class TestController {
