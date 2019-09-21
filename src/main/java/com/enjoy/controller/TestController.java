874d01a8adbb9c8b007d2441123ea9a911e3762e
bjli2018 <bjli2018@126.com>
2019-09-22 06:26:19 +0800

    del

--- src/main/java/com/enjoy/controller/TestController.java
+++ src/main/java/com/enjoy/controller/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	@RequestMapping("/hello")
 	public String hello() {
-		return "Hello Varian Medical Systems HAHAHAHA!!!";
+		return "Hello Varian Medical Systems!!!";
 	}
 }
